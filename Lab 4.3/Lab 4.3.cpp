2ceddceb4a78ed7f178027e4a593ecbaed4e3405
RomanRavlyk <Roman.Ravlyk.Ri.2023@lpnu.ua>
2023-10-10 16:06:43 +0300

    Add project files.

---
+++ Lab 4.3/Lab 4.3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, start, end, step, a, b, c, F;
+	cout << "xp = "; cin >> start;
+	cout << "xk = "; cin >> end;
+	cout << "dx = "; cin >> step;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "F" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = start;
+	while (x <= end)
+	{
+		if (a < 0 && c != 0)
+			F = a * (x * x) + b * x + c;
+		else
+			if (a > 0 && c == 0)
+				b = F = -(a) / (x - b);
+			else
+				F = a * (x + c);
+		F = a + b;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += end;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
